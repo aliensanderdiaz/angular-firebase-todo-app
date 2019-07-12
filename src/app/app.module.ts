6f4cd3657b58501ab47931acb5e67db04d45710c
aliensanderdiaz <aliensanderdiaz@gmail.com>
2019-07-12 10:51:42 -0500

    primera fase terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoFormComponent } from './todo/todo-form/todo-form.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoFormComponent,
+    MenuComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ TodoFormComponent ]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'XXXXXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXXXXX',
+    databaseURL: 'XXXXXXXXXXXXXXXXXXXX',
+    projectId: 'XXXXXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXXXXXXXXXX'
+  }
 };
 
 /*
